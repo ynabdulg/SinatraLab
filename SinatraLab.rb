003873a206ce6df70d2c3b92b3014678f30a7a6e
Yara Al Abdulghani <ynabdulg@andrew.cmu.edu>
2019-01-31 08:57:19 +0000

    done

--- SinatraLab.rb
+++ SinatraLab.rb
@@ -1,4 +1,18 @@
 require 'sinatra'
 get '/' do
-    erb :index 
+    erb :application do 
+        erb :index 
+    end 
+end
+
+
+post '/' do
+    @fname = params[:firstname] 
+    @lname = params[:lastname] 
+    @class_love = params[:class_love] 
+    @feedback = params[:feedback]
+    @happy = (@class_love == "More than I care to say") ? "That's Great!" : "Well, hopefully you'll REALLY love it by the end"
+    erb :application do 
+        erb :response 
+    end
 end
